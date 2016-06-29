9e605d1422903d27a41d0f21d8d865b9fd253765
myfstrong <4821825@qq.com>
2016-06-29 23:29:18 +0800

    HomeCampaign

--- app/src/main/java/com/myfstrong/mycniao5/bean/Campaign.java
+++ app/src/main/java/com/myfstrong/mycniao5/bean/Campaign.java
@@ -1,7 +1,36 @@
 package com.myfstrong.mycniao5.bean;
 
+import java.io.Serializable;
+
 /**
  * Created by Administrator on 2016/6/29.
  */
-public class Campaign {
+public class Campaign implements Serializable {
+    private long id;
+    private String title;
+    private String imgUrl;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
 }
---
+++ app/src/main/java/com/myfstrong/mycniao5/bean/HomeCampaign.java
@@ -0,0 +1,54 @@
+package com.myfstrong.mycniao5.bean;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 2016/6/29.
+ */
+public class HomeCampaign implements Serializable{
+    private long id;
+    private String title;
+    private Campaign cpOne;
+    private Campaign cpTwo;
+    private Campaign cpThree;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Campaign getCpOne() {
+        return cpOne;
+    }
+
+    public void setCpOne(Campaign cpOne) {
+        this.cpOne = cpOne;
+    }
+
+    public Campaign getCpTwo() {
+        return cpTwo;
+    }
+
+    public void setCpTwo(Campaign cpTwo) {
+        this.cpTwo = cpTwo;
+    }
+
+    public Campaign getCpThree() {
+        return cpThree;
+    }
+
+    public void setCpThree(Campaign cpThree) {
+        this.cpThree = cpThree;
+    }
+}
